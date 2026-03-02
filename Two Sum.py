58adbdd056124dca29295dccf832453ab04c12b3
KamilDemel <kamildemel1907@gmail.com>
2026-03-02 23:02:22 +0100

    Optimize Two Sum to O(n) using Hash Map

--- Two Sum.py
+++ Two Sum.py
@@ -1,6 +1,16 @@
+"""
 class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     return i,j
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_dict = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in num_dict:
+                return num_dict[diff],i
+            num_dict[nums[i]] = i
