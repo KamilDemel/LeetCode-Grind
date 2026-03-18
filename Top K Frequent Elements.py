77ea8a57b94a55b4d1041acecd58fecba4e56310
KamilDemel <kamildemel1907@gmail.com>
2026-03-18 04:20:38 +0100

    implement O(N) Bucket Sort for Top K Frequent Elements

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -6,3 +6,16 @@ def f(nums,k):
     for i in range(k):
         keys.append(wynik[i][0])
     return keys
+def f_otpimized(nums,k):
+    n = len(nums)
+    res = Counter(nums)
+    T = [[] for _ in range(n + 1)]
+    for key,value in res.items():
+        T[value].append(key)
+    out_Res = []
+    temp = k
+    for j in range(len(T)-1,-1,-1):
+        if temp > 0 and T[j] != []:
+            out_Res.extend(T[j])
+            temp -= len(T[j])
+    return out_Res[:k]
