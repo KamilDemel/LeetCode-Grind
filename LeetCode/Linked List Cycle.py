0a67e2baee82092dd424ad7077be8a6c70004e9b
KamilDemel <kamildemel1907@gmail.com>
2026-03-30 22:52:54 +0200

    implement floyd's cycle-finding algorithm (tortoise and hare) in linked list

---
+++ LeetCode/Linked List Cycle.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self,val,next = None):
+        self.val = val
+        self.next = next
+def sol(head):
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
