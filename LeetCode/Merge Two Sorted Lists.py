882b11007435650a814374b683422a5c9a021095
KamilDemel <kamildemel1907@gmail.com>
2026-03-30 22:26:32 +0200

    implement iterative merge of sorted linked lists using dummy node in O(1) space

---
+++ LeetCode/Merge Two Sorted Lists.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,val,next = None):
+        self.val = val
+        self.next = next
+def solution(head1,head2):
+    dummy = Node(None)
+    tail = dummy
+    while head1 and head2:
+        if head1.val >= head2.val:
+            tail.next = head1
+            tail = tail.next
+            head1 = head1.next
+        else:
+            tail.next = head2
+            tail = tail.next
+            head2 = head2.next
+    while head1:
+        tail.next = head1
+        tail = tail.next
+        head1 = head1.next
+    while head2:
+        tail.next = head2
+        tail = tail.next
+        head2 = head2.next
+    tail.next = None
+    return dummy.next
