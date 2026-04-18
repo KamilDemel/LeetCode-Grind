f8ffcd0a0415d4911390588e67c7f1a583edac73
KamilDemel <kamildemel1907@gmail.com>
2026-04-18 21:39:36 +0200

    implement 3-state DFS for cycle detection in directed graph

---
+++ LeetCode/Course Schedule.py
@@ -0,0 +1,20 @@
+def solution(prereq,n):
+    adj_list = [[] for _ in range(n)]
+    for u,v in prereq:
+        adj_list[u].append(v)
+    stany = [0] * n
+    def dfs(wezel):
+        stany[wezel] = 1
+        for sasiad in adj_list[wezel]:
+            if stany[sasiad] == 1:
+                return False
+            elif stany[sasiad] == 0:
+                if dfs(sasiad) == False:
+                    return False
+        stany[wezel] = 2
+        return True
+    for i in range(len(adj_list)):
+        if stany[i] == 0:
+            if dfs(i) == False:
+                return False
+    return True
