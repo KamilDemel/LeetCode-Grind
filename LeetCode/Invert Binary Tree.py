279468b19cd7bcbad94a6a11dde017ff3e2a24f0
KamilDemel <kamildemel1907@gmail.com>
2026-04-19 22:26:57 +0200

    solve Invert Binary Tree using recursive DFS pointer swap

---
+++ LeetCode/Invert Binary Tree.py
@@ -0,0 +1,15 @@
+class TreeNode(object):
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        def dfs(node):
+            if not node:
+                return
+            node.left,node.right = node.right,node.left
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return root
