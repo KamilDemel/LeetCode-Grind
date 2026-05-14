569e1af896432e3570c2e0ccc92dbe6e72423713
KamilDemel <kamildemel1907@gmail.com>
2026-05-14 14:03:57 +0200

    optimize climbing stairs using top-down dynamic programming (memoization)

--- LeetCode/Climbing Stairs.py
+++ LeetCode/Climbing Stairs.py
@@ -6,4 +6,16 @@ def f(n):
     for _ in range(3,n+1):
         a,b = b,a+b
     return b
-
+class Solution:
+    def climbStairs(self, n):
+        memo = {}
+        def f_reku(k):
+            if k in memo:
+                return memo[k]
+            if k == 1:
+                return 1
+            if k == 2:
+                return 2
+            memo[k] = f_reku(k - 1) + f_reku(k - 2)
+            return memo[k]
+        return f_reku(n)
