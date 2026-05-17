f30843d3af90e7a0f12fb3706713d1f96e63f6cc
KamilDemel <kamildemel1907@gmail.com>
2026-05-17 16:57:35 +0200

    implement Kadane's algorithm for maximum subarray sum (fix)

--- LeetCode/Maximum Subarray.py
+++ LeetCode/Maximum Subarray.py
@@ -1,10 +1,10 @@
-nums = [-2,1,-3,4,-1,2,1,-5,4]
-obecny_max = nums[0]
-najlepszy_wynik = nums[0]
-for i in range(1, len(nums)):
-    opcja1 = nums[i]
-    opcja2 = obecny_max + nums[i]
-    obecny_max = max(opcja1, opcja2)
-    if obecny_max > najlepszy_wynik:
-        najlepszy_wynik = obecny_max
-print(najlepszy_wynik)
+def max_subarray(nums):
+    obecny_max = nums[0]
+    najlepszy_wynik = nums[0]
+    for i in range(1, len(nums)):
+        opcja1 = nums[i]
+        opcja2 = obecny_max + nums[i]
+        obecny_max = max(opcja1, opcja2)
+        if obecny_max > najlepszy_wynik:
+            najlepszy_wynik = obecny_max
+    return najlepszy_wynik
