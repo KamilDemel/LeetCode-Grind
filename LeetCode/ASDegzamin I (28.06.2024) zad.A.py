d6cc6d1ac43fef231e53e648c76c2fbde7c0aec0
KamilDemel <kamildemel1907@gmail.com>
2026-06-20 22:15:54 +0200

    optimize Armstrong pathfinding using dual Dijkstra approach

--- LeetCode/ASDegzamin I (28.06.2024) zad.A.py
+++ LeetCode/ASDegzamin I (28.06.2024) zad.A.py
@@ -1,4 +1,5 @@
 import heapq
+"""
 def armstrong(B, G, s, t):
     max_V = max(max(u,v) for u,v,w in G)
     adj_list = [[] for _ in range(max_V + 1)]
@@ -32,4 +33,39 @@ def armstrong(B, G, s, t):
         wyniki.append(distances[0][t])
         wyniki.append(distances[1][t])
     return min(wyniki)
-
+"""
+def armstrong(B,G,s,t):
+    max_V = max(max(u,v) for u,v,w in G)
+    adj_list = [[] for _ in range(max_V + 1)]
+    for u,v,w in G:
+        adj_list[u].append((w,v))
+        adj_list[v].append((w,u))
+    distances_od_s = [float("inf")] * (max_V + 1)
+    distances_od_t = [float("inf")] * (max_V + 1)
+    distances_od_s[s] = 0
+    distances_od_t[t] = 0
+    pq1 = [(0,s)]
+    while pq1:
+        curr_distance, curr_v = heapq.heappop(pq1)
+        if curr_distance > distances_od_s[curr_v]:
+            continue
+        for waga, sasiad in adj_list[curr_v]:
+            new_distanc = waga + curr_distance
+            if new_distanc < distances_od_s[sasiad]:
+                distances_od_s[sasiad] = new_distanc
+                heapq.heappush(pq1,(new_distanc,sasiad))
+    pq2 = [(0,t)]
+    while pq2:
+        curr_distance, curr_v = heapq.heappop(pq2)
+        if curr_distance > distances_od_t[curr_v]:
+            continue
+        for waga, sasiad in adj_list[curr_v]:
+            new_distanc = waga + curr_distance
+            if new_distanc < distances_od_t[sasiad]:
+                distances_od_t[sasiad] = new_distanc
+                heapq.heappush(pq2,(new_distanc,sasiad))
+    best_czas = distances_od_s[t]
+    for idx_v,p,q in B:
+        czas = distances_od_s[idx_v] + (distances_od_t[idx_v] * (p/q))
+        best_czas = min(best_czas,czas)
+    return best_czas
