6464a212bf910d300078af38fa91ba75598fc975
KamilDemel <kamildemel1907@gmail.com>
2026-06-23 18:10:17 +0200

    implement O(n^4) top-down DP with custom memoization for snow collection

--- LeetCode/ASDegzamin I (07.07.2022) zad.A.py
+++ LeetCode/ASDegzamin I (07.07.2022) zad.A.py
@@ -6,3 +6,21 @@ def solve(S):
             break
         wynik = wynik + (S[i] - i)
     return wynik
+
+def snow_dp(S):
+    n = len(S)
+    memo = {}
+    def solve(L, R, dzien):
+        if (L,R,dzien) in memo:
+            return memo[(L,R,dzien)]
+        if L > R or dzien >= n:
+            return 0
+        najlepszy_wynik = 0
+        for k in range(L, R + 1):
+            ile_zbierzemy = max(0, S[k] - dzien)
+            wjazd_zachod = ile_zbierzemy + solve(k + 1, R, dzien + 1)
+            wjazd_wschod = ile_zbierzemy + solve(L, k - 1, dzien + 1)
+            najlepszy_wynik = max(najlepszy_wynik, wjazd_zachod, wjazd_wschod)
+        memo[(L,R,dzien)] = najlepszy_wynik
+        return najlepszy_wynik
+    return solve(0, n - 1, 0)
